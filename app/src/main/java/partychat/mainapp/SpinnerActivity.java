ec500b721c76d22600a4b542cf3464bf072e0785
SamSindelar <samsindelar.ss@gmail.com>
2018-12-06 00:54:18 -0600

    Create SpinnerActivity.java

---
+++ app/src/main/java/partychat/mainapp/SpinnerActivity.java
@@ -0,0 +1,36 @@
+package partychat.mainapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.*;
+
+
+
+public class SpinnerActivity extends ChatroomList implements OnItemSelectedListener {
+
+
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+}
