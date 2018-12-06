e91797ab807e417e03ffa3916041952268603788
SamSindelar <samsindelar.ss@gmail.com>
2018-12-06 00:53:56 -0600

    Update ChatroomList.java

--- app/src/main/java/partychat/mainapp/ChatroomList.java
+++ app/src/main/java/partychat/mainapp/ChatroomList.java
@@ -11,11 +11,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 import java.io.IOException;
 
 public class ChatroomList extends AppCompatActivity {
     private RecyclerView recyclerView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +30,18 @@ public class ChatroomList extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setupRecyclerView(ChatroomAdapter.count);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.menu_spinner1);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.menu_spinner_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+
+
+
     }
 
     @Override
